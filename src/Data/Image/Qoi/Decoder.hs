50695b7300ffb0c6b6fa42dc82f96d5c4de9dca3
0xd34df00d <0xd34df00d@gmail.com>
2021-12-04 13:50:05 -0600

    Generalize decoding to both RGBA and RGB pixels

--- src/Data/Image/Qoi/Decoder.hs
+++ src/Data/Image/Qoi/Decoder.hs
@@ -32,7 +32,7 @@ data ChunkResult pixel
   | Repeat pixel Int
   | Lookback Int
 
-peekChunk :: BS.ByteString -> Int -> Pixel3 -> (Int, ChunkResult Pixel3)
+peekChunk :: Pixel pixel => BS.ByteString -> Int -> pixel -> (Int, ChunkResult pixel)
 peekChunk str pos prevPixel
   | byte .>>. 6 == 0     = (1, Lookback $ fromIntegral $ byte .&. 0b00111111)
   | byte .>>. 5 == 0b010 = (1, Repeat prevPixel $ fromIntegral $ 1 + byte .&. 0b00011111)
@@ -43,12 +43,12 @@ peekChunk str pos prevPixel
   | byte .>>. 6 == 0b10  = let dr = (byte .>>. 4 .&. 0b11) - 2
                                dg = (byte .>>. 2 .&. 0b11) - 2
                                db = (byte        .&. 0b11) - 2
-                            in (1, One $ prevPixel `addPixel3` Pixel3 dr dg db)
+                            in (1, One $ addRGB prevPixel dr dg db)
   | byte .>>. 5 == 0b110 = let next = str ! pos + 1
                                dr = (byte .&. 0b00011111) - 16
                                dg = (next .>>. 4)         - 8
                                db = (next .&. 0b00001111) - 8
-                            in (2, One $ prevPixel `addPixel3` Pixel3 dr dg db)
+                            in (2, One $ addRGB prevPixel dr dg db)
   | byte .>>. 4 == 0b1110 = let threeBytes :: Word32
                                 threeBytes = fromIntegral byte .<<. 16
                                          .|. fromIntegral (str ! pos + 1) .<<. 8
@@ -56,56 +56,57 @@ peekChunk str pos prevPixel
                                 dr = fromIntegral (threeBytes .>>. 15 .&. 0b11111) - 16
                                 dg = fromIntegral (threeBytes .>>. 10 .&. 0b11111) - 16
                                 db = fromIntegral (threeBytes .>>. 5  .&. 0b11111) - 16
-                                --da = fromIntegral (threeBytes         .&. 0b11111) - 16
-                             in (3, One $ prevPixel `addPixel3` Pixel3 dr dg db)
+                                da = fromIntegral (threeBytes         .&. 0b11111) - 16
+                             in (3, One $ addRGBA prevPixel dr dg db da)
   | byte .>>. 4 == 0b1111 = let hr = byte .>>. 3 .&. 0b1
                                 hg = byte .>>. 2 .&. 0b1
                                 hb = byte .>>. 1 .&. 0b1
                                 ha = byte        .&. 0b1
-                                Pixel3 r g b = prevPixel
+                                (r, g, b, a) = toRGBA prevPixel
                                 r' = (negate hr .&. (str ! pos + 1))
                                  .|. (hr - 1)    .&. r
                                 g' = (negate hg .&. (str ! pos + 1 + fromIntegral hr))
                                  .|. (hg - 1)    .&. g
                                 b' = (negate hb .&. (str ! pos + 1 + fromIntegral (hr + hg)))
                                  .|. (hb - 1)    .&. b
-                                --a = negate ha .&. (str ! pos + 1 + fromIntegral (hr + hg + hb))
-                             in (1 + fromIntegral (hr + hg + hb + ha), One $ Pixel3 r' g' b')
+                                a' = (negate ha .&. (str ! pos + 1 + fromIntegral (hr + hg + hb)))
+                                 .|. (ha - 1)    .&. a
+                             in (1 + fromIntegral (hr + hg + hb + ha), One $ fromRGBA r' g' b' a')
   | otherwise = error "unknown byte"
   where
     byte = str ! pos
 
-
-decode3ch :: BS.ByteString -> Int -> V.Vector Pixel3
-decode3ch str n = V.create $ do
+decodePixels :: Pixel pixel => BS.ByteString -> Int -> V.Vector pixel
+decodePixels str n = V.create $ do
   mvec <- VM.new n
 
-  running <- VM.replicate 64 $ Pixel3 0 0 0
+  running <- VM.replicate 64 $ initPixel
 
-  let updateRunning3 px@(Pixel3 r g b) = VM.unsafeWrite running (fromIntegral $ (r `xor` g `xor` b `xor` 255) .&. 0b00111111) px
+  let updateRunning px = let (r, g, b, a) = toRGBA px
+                          in VM.unsafeWrite running (fromIntegral $ (r `xor` g `xor` b `xor` a) .&. 0b00111111) px
 
   let step inPos outPos prevPixel
         | outPos < n = do
             let (diff, chunk) = peekChunk str inPos prevPixel
             case chunk of
                  One px        -> do VM.unsafeWrite mvec outPos px
-                                     updateRunning3 px
+                                     updateRunning px
                                      step (inPos + diff) (outPos + 1)   px
                  Lookback pos  -> do px <- VM.unsafeRead running pos
                                      VM.unsafeWrite mvec outPos px
                                      step (inPos + diff) (outPos + 1)   px
                  Repeat px cnt -> do VM.set (VM.unsafeSlice outPos cnt mvec) px
-                                     updateRunning3 px
+                                     updateRunning px
                                      step (inPos + diff) (outPos + cnt) px
         | otherwise = pure ()
-  step 0 0 (Pixel3 0 0 0)
+  step 0 0 initPixel
 
   pure mvec
 
 decodeQoi :: BS.ByteString -> Maybe (Header, V.Vector Pixel3)
 decodeQoi str
   | consumed /= 14 = Nothing
-  | otherwise = Just (header, decode3ch (BS.drop consumed str) (fromIntegral $ hWidth header * hHeight header))
+  | otherwise = Just (header, decodePixels (BS.drop consumed str) (fromIntegral $ hWidth header * hHeight header))
   where
     (consumed, header) = second fixupBE $ decodeExPortionWith peek str
     fixupBE Header { .. } = Header { hMagic = fromBigEndian hMagic
--- src/Data/Image/Qoi/Pixel.hs
+++ src/Data/Image/Qoi/Pixel.hs
@@ -6,16 +6,44 @@
 
 module Data.Image.Qoi.Pixel where
 
+import qualified Data.Vector.Unboxed.Mutable as VM
 import Data.Vector.Unboxed.Deriving
 import Data.Word
 
 data Pixel3 = Pixel3 Word8 Word8 Word8 deriving (Show)
 data Pixel4 = Pixel4 Word8 Word8 Word8 Word8 deriving (Show)
 
-addPixel3 :: Pixel3 -> Pixel3 -> Pixel3
-addPixel3 (Pixel3 r1 g1 b1) (Pixel3 r2 g2 b2) = Pixel3 (r1 + r2) (g1 + g2) (b1 + b2)
-
 derivingUnbox "Pixel3"
   [t| Pixel3 -> (Word8, Word8, Word8) |]
   [| \(Pixel3 r g b) -> (r, g, b) |]
   [| \(r, g, b) -> Pixel3 r g b |]
+
+derivingUnbox "Pixel4"
+  [t| Pixel4 -> (Word8, Word8, Word8, Word8) |]
+  [| \(Pixel4 r g b a) -> (r, g, b, a) |]
+  [| \(r, g, b, a) -> Pixel4 r g b a |]
+
+class VM.Unbox a => Pixel a where
+  initPixel :: a
+
+  addRGB  :: a -> Word8 -> Word8 -> Word8 -> a
+  addRGBA :: a -> Word8 -> Word8 -> Word8 -> Word8 -> a
+
+  toRGBA :: a -> (Word8, Word8, Word8, Word8)
+  fromRGBA :: Word8 -> Word8 -> Word8 -> Word8 -> a
+
+instance Pixel Pixel3 where
+  initPixel = Pixel3 0 0 0
+
+  addRGB (Pixel3 r g b) dr dg db = Pixel3 (r + dr) (g + dg) (b + db)
+  addRGBA (Pixel3 r g b) dr dg db _ = Pixel3 (r + dr) (g + dg) (b + db)
+  toRGBA (Pixel3 r g b) = (r, g, b, 255)
+  fromRGBA r g b _ = Pixel3 r g b
+
+instance Pixel Pixel4 where
+  initPixel = Pixel4 0 0 0 255
+
+  addRGB (Pixel4 r g b a) dr dg db = Pixel4 (r + dr) (g + dg) (b + db) a
+  addRGBA (Pixel4 r g b a) dr dg db da = Pixel4 (r + dr) (g + dg) (b + db) (a + da)
+  toRGBA (Pixel4 r g b a) = (r, g, b, a)
+  fromRGBA r g b a = Pixel4 r g b a
