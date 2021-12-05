7036009adb9089c9a24afc18d6863b288457cd86
0xd34df00d <0xd34df00d@gmail.com>
2021-12-05 10:01:45 -0600

    Store pixels sequentially in memory
    Kinda provides about 7% boost.

--- src/Data/Image/Qoi/Pixel.hs
+++ src/Data/Image/Qoi/Pixel.hs
@@ -1,27 +1,69 @@
-{-# LANGUAGE StrictData #-}
+{-# LANGUAGE Strict #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE BangPatterns #-}
+{-# OPTIONS_GHC -O2 -fllvm #-}
 
 module Data.Image.Qoi.Pixel where
 
+import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as VM
+import qualified Data.Vector.Generic.Base as VG
+import qualified Data.Vector.Generic.Mutable.Base as VMG
 import Data.Vector.Unboxed.Deriving
+import Data.Bits
 import Data.Word
 
 data Pixel3 = Pixel3 Word8 Word8 Word8 deriving (Show)
 data Pixel4 = Pixel4 Word8 Word8 Word8 Word8 deriving (Show)
 
-derivingUnbox "Pixel3"
-  [t| Pixel3 -> (Word8, Word8, Word8) |]
-  [| \(Pixel3 r g b) -> (r, g, b) |]
-  [| \(r, g, b) -> Pixel3 r g b |]
+newtype instance VM.MVector s Pixel3 = MV_Pixel3 { getMVP3 :: VM.MVector s Word8 }
+newtype instance V.Vector     Pixel3 = V_Pixel3  { getVP3  :: V.Vector Word8 }
+
+instance VG.Vector V.Vector Pixel3 where
+  basicUnsafeFreeze = fmap V_Pixel3 . VG.basicUnsafeFreeze . getMVP3
+  basicUnsafeThaw = fmap MV_Pixel3 . VG.basicUnsafeThaw . getVP3
+  basicLength = (`div` 3) . VG.basicLength . getVP3
+  basicUnsafeSlice s l = V_Pixel3 . VG.basicUnsafeSlice (s * 3) (l * 3) . getVP3
+  basicUnsafeIndexM (V_Pixel3 vec) idx = Pixel3 <$> VG.basicUnsafeIndexM vec idx'
+                                                <*> VG.basicUnsafeIndexM vec (idx' + 1)
+                                                <*> VG.basicUnsafeIndexM vec (idx' + 2)
+    where
+      idx' = idx * 3
+  elemseq _ !px b = b
+
+instance VMG.MVector VM.MVector Pixel3 where
+  basicLength = (`div` 3) . VMG.basicLength . getMVP3
+  basicUnsafeSlice s l = MV_Pixel3 . VMG.basicUnsafeSlice (s * 3) (l * 3) . getMVP3
+  basicOverlaps (MV_Pixel3 v1) (MV_Pixel3 v2) = VMG.basicOverlaps v1 v2
+  basicUnsafeNew = fmap MV_Pixel3 . VMG.basicUnsafeNew . (* 3)
+  basicInitialize = VMG.basicInitialize . getMVP3
+  basicUnsafeRead (MV_Pixel3 vec) idx = Pixel3 <$> VMG.basicUnsafeRead vec idx'
+                                               <*> VMG.basicUnsafeRead vec (idx' + 1)
+                                               <*> VMG.basicUnsafeRead vec (idx' + 2)
+    where
+      idx' = idx * 3
+  basicUnsafeWrite (MV_Pixel3 vec) idx (Pixel3 r g b) = VMG.basicUnsafeWrite vec idx' r
+                                                     >> VMG.basicUnsafeWrite vec (idx' + 1) g
+                                                     >> VMG.basicUnsafeWrite vec (idx' + 2) b
+    where
+      idx' = idx * 3
+
+instance VM.Unbox Pixel3
 
 derivingUnbox "Pixel4"
-  [t| Pixel4 -> (Word8, Word8, Word8, Word8) |]
-  [| \(Pixel4 r g b a) -> (r, g, b, a) |]
-  [| \(r, g, b, a) -> Pixel4 r g b a |]
+  [t| Pixel4 -> Word32 |]
+  [| \(Pixel4 r g b a) -> (fromIntegral r `shiftL` 24)
+                      .|. (fromIntegral g `shiftL` 16)
+                      .|. (fromIntegral b `shiftL` 8)
+                      .|.  fromIntegral a
+                      |]
+  [| \w32 -> Pixel4 (fromIntegral $ w32 `shiftR` 24)
+                    (fromIntegral $ w32 `shiftR` 16)
+                    (fromIntegral $ w32 `shiftR` 8)
+                    (fromIntegral   w32)
+                    |]
 
 class VM.Unbox a => Pixel a where
   initPixel :: a
