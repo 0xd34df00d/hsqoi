674d03556ae3ac2fee093c3bac1f05fd91292476
0xd34df00d <0xd34df00d@gmail.com>
2021-12-12 13:18:24 -0600

    Generalize tests for 4-channel images as well

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE Strict #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE MonoLocalBinds #-}
 
 import qualified Data.Array.IArray as A
 import qualified Data.Array.Unboxed as A
@@ -9,6 +15,7 @@ import qualified Data.Vector as V
 import Control.Monad
 import Data.Binary.Combinators
 import Data.Either
+import Data.Proxy
 import Data.Word
 import Test.Hspec
 import Test.QuickCheck
@@ -19,61 +26,72 @@ import Data.Image.Qoi.Encoder
 import Data.Image.Qoi.Format
 import Data.Image.Qoi.Pixel
 
-newtype ShowAsBytes = ShowAsBytes { bytes :: BS.ByteString } deriving (Eq)
+newtype ShowAsBytes pixel = ShowAsBytes { bytes :: BS.ByteString } deriving (Eq)
 
-instance Show ShowAsBytes where
-  show = show . toArray3 . bytes
+instance (Show pixel, A.IArray A.UArray pixel, Pixel pixel) => Show (ShowAsBytes pixel) where
+  show = show . toArray @pixel . bytes
 
-data Image3 = Image3
+data Image pixel = Image
   { iWidth :: Int
   , iHeight :: Int
-  , iBytes :: ShowAsBytes
-  } deriving (Eq, Show)
+  , iBytes :: ShowAsBytes pixel
+  } deriving (Eq)
 
-genPixel3 :: Gen Pixel3
-genPixel3 = Pixel3 <$> chooseAny <*> chooseAny <*> chooseAny
+deriving instance Show (ShowAsBytes pixel) => Show (Image pixel)
 
-genDiff3Bounded :: Word8 -> Pixel3 -> Gen Pixel3
-genDiff3Bounded delta (Pixel3 r g b) = do
-  diffs <- replicateM 3 $ choose (negate delta, delta - 1)
+genPixel :: forall pixel. Pixel pixel => Gen pixel
+genPixel = fromRGBA <$> chooseAny <*> chooseAny <*> chooseAny <*> if channelCount @pixel Proxy == 3 then pure 0 else chooseAny
+
+genDiffBounded :: forall pixel. Pixel pixel => Bool -> Word8 -> pixel -> Gen pixel
+genDiffBounded withAlpha delta px = do
+  diffs <- replicateM (if withAlpha then channelCount @pixel Proxy else 3) $ choose (negate delta, delta - 1)
   case diffs of
-       [dr, dg, db] -> pure $ Pixel3 (r + dr) (g + dg) (b + db)
+       [dr, dg, db]     -> pure $ addRGB  px dr dg db
+       [dr, dg, db, da] -> pure $ addRGBA px dr dg db da
        _ -> error "muh dependent types"
 
-instance Arbitrary Image3 where
+instance Pixel pixel => Arbitrary (Image pixel) where
   arbitrary = do
     maxDim <- getSize
     width <- chooseInt (1, maxDim)
     height <- chooseInt (1, maxDim)
 
-    px0 <- genPixel3
+    px0 <- genPixel
 
     pixels <- V.iterateNM (width * height) step px0
-    pure $ Image3 width height $ ShowAsBytes $ BS.pack $ V.toList $ V.concatMap (\(Pixel3 r g b) -> [r, g, b]) pixels
+    pure $ Image width height $ ShowAsBytes $ BS.pack $ V.toList $ V.concatMap toComponents pixels
     where
+      toComponents :: pixel -> V.Vector Word8
+      toComponents px = if channelCount @pixel Proxy == 3
+                           then let (r, g, b, _) = toRGBA px in [r, g, b]
+                           else let (r, g, b, a) = toRGBA px in [r, g, b, a]
       step prevPixel = oneof [ pure prevPixel
-                             , genDiff3Bounded 2 prevPixel
-                             , genDiff3Bounded 8 prevPixel
-                             , genDiff3Bounded 16 prevPixel
-                             , genPixel3
+                             , genDiffBounded False 2 prevPixel
+                             , genDiffBounded False 8 prevPixel
+                             , genDiffBounded True 16 prevPixel
+                             , genPixel
                              ]
 
-toArray3 :: BS.ByteString -> A.UArray Int Pixel3
-toArray3 bs = A.array (0, pxCnt - 1) [ (i, Pixel3 r g b)
-                                     | i <- [0..pxCnt - 1]
-                                     , let r = bs `BS.index` (i * 3)
-                                           g = bs `BS.index` (i * 3 + 1)
-                                           b = bs `BS.index` (i * 3 + 2)
-                                     ]
+toArray :: forall pixel. (Pixel pixel, A.IArray A.UArray pixel) => BS.ByteString -> A.UArray Int pixel
+toArray bs = A.array (0, pxCnt - 1) [ (i, fromRGBA r g b a)
+                                    | i <- [0..pxCnt - 1]
+                                    , let r = bs `BS.index` (i * chans)
+                                          g = bs `BS.index` (i * chans + 1)
+                                          b = bs `BS.index` (i * chans + 2)
+                                          a = if chans == 4
+                                                 then bs `BS.index` (i * chans + 3)
+                                                 else 0
+                                    ]
   where
-    pxCnt = BS.length bs `div` 3
+    chans = channelCount @pixel Proxy
+    pxCnt = BS.length bs `div` chans
 
-imgProperty :: Image3 -> IO ()
-imgProperty Image3 { .. } = do
+imgProperty :: forall pixel. (Show pixel, Pixel pixel, A.IArray A.UArray pixel) => Image pixel -> IO ()
+imgProperty Image { .. } = do
   let header = Header { hMagic = matchBytes
                       , hWidth = fromIntegral iWidth
                       , hHeight = fromIntegral iHeight
-                      , hChannels = 3
+                      , hChannels = chans
                       , hColorspace = 0
                       }
   let encoded = encodeRaw header (bytes iBytes) 0
@@ -82,12 +100,23 @@ imgProperty Image3 { .. } = do
   case decoded of
        Left _ -> pure ()
        Right (header', Pixels3 pixels') -> do
+         chans `shouldBe` 3
          header' `shouldBe` header
-         pixels' `shouldBe` toArray3 (bytes iBytes)
-       Right _ -> fail "Expected Pixels3"
+         pixels' `shouldBe` toArray (bytes iBytes)
+       Right (header', Pixels4 pixels') -> do
+         chans `shouldBe` 4
+         header' `shouldBe` header
+         pixels' `shouldBe` toArray (bytes iBytes)
+  where
+    chans = fromIntegral $ channelCount @pixel Proxy
 
 main :: IO ()
-main = hspec $ modifyMaxSuccess (const 100000) $
-  describe "QOI encoder" $
-    parallel $ forM_ ([1..10] :: [Int]) $ \wrk ->
-      it ("decode . encode = id (worker " <> show wrk <> ")") $ property imgProperty
+main = hspec $ modifyMaxSuccess (const 100000) $ do
+  describe "QOI encoder (3-channel)" $
+    parallel $ forM_ ([1 .. workers] :: [Int]) $ \wrk ->
+      it ("decode . encode = id (worker " <> show wrk <> ")") $ property (imgProperty @Pixel3)
+  describe "QOI encoder (4-channel)" $
+    parallel $ forM_ ([1 .. workers] :: [Int]) $ \wrk ->
+      it ("decode . encode = id (worker " <> show wrk <> ")") $ property (imgProperty @Pixel4)
+  where
+    workers = 10
